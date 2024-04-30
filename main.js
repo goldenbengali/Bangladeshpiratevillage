8945ab0742bf9f925aa7f9d4d074a16893d31f29
goldenbengali <168433973+goldenbengali@users.noreply.github.com>
2024-04-30 20:54:57 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+if (isMobile) {
+    alert("For the best experience, please use desktop mode.");
+} else {
+}
